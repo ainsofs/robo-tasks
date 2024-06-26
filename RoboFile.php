d8eb1e2b99804ce66cfbe0684d1ac702809060fc
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-26 16:08:45 +1300

    add default argument and allow drupal version override

--- RoboFile.php
+++ RoboFile.php
@@ -9,11 +9,12 @@ class RoboFile extends Tasks
      * Create a new Git tag by incrementing the latest tag.
      *
      * @param string $increment The part of the version to increment (patch, minor, major).
+     * @param string|null $drupalVersion Optional specific Drupal version to override.
      */
-    public function createTag($increment = 'patch')
+    public function createTag($increment = 'patch', $drupalVersion = null)
     {
         $tagManager = new TagManager();
-        $tagManager->createTag($increment);
+        $tagManager->createTag($increment, $drupalVersion);
     }
 }
 
